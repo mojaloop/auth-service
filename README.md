adce2eda692009d18951e79c7b6d63acf487b71c
Lewis Daly <lewisdaly@me.com>
2020-05-13 18:39:38 +0800

    Initial commit, based on template-typescript-public repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# Auth Service (Work in Progress)
+[![Git Commit](https://img.shields.io/github/last-commit/mojaloop/auth-service.svg?style=flat)](https://github.com/mojaloop/auth-service/commits/master)
+[![Git Releases](https://img.shields.io/github/release/mojaloop/auth-service.svg?style=flat)](https://github.com/mojaloop/auth-service/releases)
+[![Npm Version](https://img.shields.io/npm/v/@mojaloop/auth-service.svg?style=flat)](https://www.npmjs.com/package/@mojaloop/auth-service)
+[![NPM Vulnerabilities](https://img.shields.io/snyk/vulnerabilities/npm/@mojaloop/auth-service.svg?style=flat)](https://www.npmjs.com/package/@mojaloop/auth-service)
+[![CircleCI](https://circleci.com/gh/mojaloop/auth-service.svg?style=svg)](https://circleci.com/gh/mojaloop/auth-service)
+
+Mojaloop central AuthZ + AuthN service. Currently for FIDO implementation in a Mojaloop switch.
+
+## Overview
+
+- [Documentation](./docs/README.md)
+
+## Setup
+
+
+
---
+++ test/unit/index.test.ts
@@ -0,0 +1,46 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ * Lewis Daly <lewis@vesselstech.com>
+
+ --------------
+ ******/
+
+'use strict'
+
+let Test = require('tapes')(require('tape'))
+
+Test('Example Test', (exampleTest: any): void => {
+  exampleTest.test('Adds two numbers together', (test: any): void => {
+    // Arrange
+    const input1 = 10
+    const input2 = 20
+    const expected = 30
+
+    // Act
+    const result = input1 + input2
+
+    //Assert
+    test.ok(result, expected, 'Result matches expectation')
+    test.end()
+  })
+
+  exampleTest.end()
+})
