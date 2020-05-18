15345a8a973e41bb430276f60de1047857fd1b8e
Paweł Marzec <pm@eoln.eu>
2020-05-18 13:23:03 +0200

    make jest config a little more usefull

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 'use strict'
 
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverage: false,
+  collectCoverage: true,
   collectCoverageFrom: ['./src/**/*.ts'],
-  coverageReporters: ['json', 'lcov'],
-  clearMocks: true
+  coverageReporters: ['json', 'lcov', 'text'],
+  clearMocks: true,
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      functions: 90,
+      branches: 90,
+      lines: 90
+    }
+  }
 }
