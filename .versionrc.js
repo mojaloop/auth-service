a58dbf2d44693e10167d1a3aab74dbbd3af293e5
Paweł Marzec <pm@eoln.eu>
2020-05-22 13:45:34 +0200

    chore(.version.js): update header

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  header: 'Changelog: [mojaloop/auth-service](https://github.com/mojaloop/auth-service)',
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'chore', hidden: true },
+    { type: 'ci', hidden: true },
+    { type: 'docs', hidden: true },
+    { type: 'style', hidden: true },
+    { type: 'refactor', hidden: true },
+    { type: 'perf', hidden: true },
+    { type: 'test', hidden: true }
+  ]
+}
