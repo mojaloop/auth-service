70bd0bad06d1a6b0a7b2d34fceb88f738de9ba39
spikerheado1234 <ahangupta.96@gmail.com>
2020-06-24 15:55:36 +0800

    Set up knex connection and migrations folder

---
+++ config/knexfile.js
@@ -0,0 +1,19 @@
+const migrationsDirectory = "../migrations"
+const seedsDirectory = "../seeds"
+const Config = require('../src/lib/config')
+
+module.exports = {
+  client: 'mysql',
+  version: '2.18',
+  connection: Config.DATABASE.connection,
+  pool: Config.DATABASE.pool,
+  migrations: {
+    directory: migrationsDirectory,
+    tableName: 'auth-service',
+    stub: `${migrationsDirectory}/migration.template`
+  },
+  seeds: {
+    directory: seedsDirectory,
+    loadExtensions: ['.js']
+  }
+}
