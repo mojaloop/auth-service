1fce7276ffeae9333707f759a97cbe9140909a76
spikerheado1234 <ahangupta.96@gmail.com>
2020-06-26 11:53:11 +0800

    Set up dev env. in knexfile

--- config/knexfile.ts
+++ config/knexfile.ts
@@ -3,17 +3,19 @@ const seedsDirectory = "../seeds"
 import {config} from "../src/lib/config"
 
 module.exports = {
-  client: 'mysql',
-  version: '5.5',
-  connection: config.DATABASE.connection,
-  pool: config.DATABASE.pool,
-  migrations: {
-    directory: migrationsDirectory,
-    tableName: 'auth-service',
-    stub: `${migrationsDirectory}/migration.template`
-  },
-  seeds: {
-    directory: seedsDirectory,
-    loadExtensions: ['.js']
+  development : {
+    client: 'mysql',
+    version: '5.5',
+    connection: config.DATABASE.connection,
+    pool: config.DATABASE.pool,
+    migrations: {
+      directory: migrationsDirectory,
+      tableName: 'auth-service',
+      stub: `${migrationsDirectory}/migration.template`
+    },
+    seeds: {
+      directory: seedsDirectory,
+      loadExtensions: ['.js']
+    } 
   }
 }
