a216ac062e9d2327bbbaabd5d3b26c14c26e0236
akapur99 <abhi.kapur09@gmail.com>
2020-07-02 14:22:34 +0530

    POST method to handle challenge generation request Relies on consent models so currently has unmet dependencies. Basic error handling but doesn't check credential type.

---
+++ src/server/handlers/consents/{ID}/generateChallenge.ts
@@ -0,0 +1,55 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the 'License') and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ - Abhimanyu Kapur <abhi.kapur09@gmail.com>
+
+ --------------
+ ******/
+import { genChallenge, isConsentRequestValid } from '../../../domain/consents/{ID}/generateChallenge'
+import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi'
+import { consentDB } from '../../../../lib/db'
+import { Consent } from '../../../../model/consent'
+
+/** The HTTP request `POST /consents/{ID}/generateChallenge` is used to create a
+ * credential for the given Consent object. The `{ID}` in the URI should
+ * contain the `{ID}` that was used in the `POST /consents`.
+ * Called by a `PISP`to request a challenge from the `auth-service`, which
+ * will be returned to the PISP via `PUT /consents/{ID}`
+ */
+export function post (_: Request, h: ResponseToolkit): ResponseObject {
+  const id = _.params.ID
+
+  // Fetch consent using ID
+  let consent: Consent = null
+  try {
+    consent = consentDB.retrieve(id)
+  } catch (error) {
+    throw new Error('Invalid Consent Lookup')
+  }
+
+  // If consent is valid, generate challenge asynchronously
+  // and return 202 Success code
+  if (isConsentRequestValid(_, consent)) {
+    genChallenge(_, consent)
+    return h.response().code(202)
+  } else {
+    throw new Error('400')
+  }
+}
