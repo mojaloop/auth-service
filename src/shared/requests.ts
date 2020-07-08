3dff464928ed18d288a20b2ba542ff0f31b9da28
akapur99 <abhi.kapur09@gmail.com>
2020-07-08 10:45:19 +0530

    <refactor+style> Change scope declaration + added comments/licence

--- src/shared/requests.ts
+++ src/shared/requests.ts
@@ -1,11 +1,38 @@
-// mockup sdk-standard-components library out req func
+/*****
+ License
+ --------------
+ Copyright © 2020 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the 'License') and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
 
+ - Abhimanyu Kapur <abhi.kapur09@gmail.com>
+
+ --------------
+ ******/
 import { putConsents } from '@mojaloop/sdk-standard-components'
 import { Consent } from '../model/consent'
 import { Scope } from '../model/scope'
 import { scopeDb } from '../lib/db'
 const Enum = require('@mojaloop/central-services-shared').Enum
 
+/**
+ * Builds body of outgoing request and makes PUT consents/{ID} call to server
+ * @param consent Consent object with credential challenge, type and status
+ * @param headers headers from PISP generate challenge request
+ */
 export async function putConsentId (consent: Consent, headers): Promise<void> {
   // Switch SOURCE and DESTINATION in headers
   const destinationId = headers[Enum.Http.Headers.FSPIOP.SOURCE]
@@ -13,8 +40,7 @@ export async function putConsentId (consent: Consent, headers): Promise<void> {
   headers[Enum.Http.Headers.FSPIOP.DESTINATION] = destinationId
 
   // Retrieve scopes
-  let scopes: Scope = null
-  scopes = await scopeDb.retrieve(consent.id)
+  const scopes = await scopeDb.retrieve(consent.id)
 
   // Construct body of outgoing request
   const body = {
@@ -35,4 +61,6 @@ export async function putConsentId (consent: Consent, headers): Promise<void> {
   }
   // Use sdk-standard-components library to send request
   putConsents(body, destinationId, consent.id, headers)
+
+  // TODO: Figure if I need to return something here or deal with anything
 }
