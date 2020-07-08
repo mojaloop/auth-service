6a60a343b930cd0d0362b1dcd8f34bd7fd1d1df3
akapur99 <abhi.kapur09@gmail.com>
2020-07-08 10:55:54 +0530

    <refactor> Modified error handling + Logger message

--- src/server/domain/consents/{ID}/generateChallenge.ts
+++ src/server/domain/consents/{ID}/generateChallenge.ts
@@ -1,7 +1,7 @@
 /*****
  License
  --------------
- Copyright © 20202 Mojaloop Foundation
+ Copyright © 2020 Mojaloop Foundation
  The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the 'License') and you may not use these files except in compliance with the License. You may obtain a copy of the License at
  http://www.apache.org/licenses/LICENSE-2.0
  Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
@@ -56,7 +56,8 @@ export async function generateChallengeValue (): Promise<string> {
     const challenge = buf.toString('base64')
     return challenge
   } catch (error) {
-    throw new Error('Error in Generating Challenge Value')
+    Logger.error('Unable to generate challenge string')
+    throw error
   }
 }
 
