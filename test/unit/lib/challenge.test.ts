072b56fd140ce2f8657e99b1e17bc26012bfed36
akapur99 <abhi.kapur09@gmail.com>
2020-07-09 11:21:50 +0530

    <feat> Unit tests for challenge generation

---
+++ test/unit/lib/challenge.test.ts
@@ -0,0 +1,13 @@
+import { generate } from "../../../src/lib/challenge"
+
+describe('Challenge Generation', (): void => {
+  it('Should return a 32 byte string', async (): Promise<void> => {
+    const challenge = await generate()
+    expect(Buffer.byteLength(challenge, 'base64')).toBe(32)
+  })
+
+  it('Should return a 64 byte string', async (): Promise<void> => {
+    const challenge = await generate(64)
+    expect(Buffer.byteLength(challenge, 'base64')).toBe(32)
+  })
+})
