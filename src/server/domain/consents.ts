53ec5632d8f4dd03f1a530ad00ffcad10c8e6c26
akapur99 <abhi.kapur09@gmail.com>
2020-07-13 13:55:41 +0530

    <feat> Implement parsing and storing scopes

--- src/server/domain/consents.ts
+++ src/server/domain/consents.ts
@@ -14,9 +14,22 @@ export async function createAndStoreConsent (request: Request): Promise<void> {
     participantId: request.params.participantId
   }
 
+  const scopesArray: Scope[] = []
+
+  payload.scopes.forEach((element: object): void => {
+    const accountId = element.accountId
+    element.actions.forEach((action: object): void => {
+      const scope = {
+        accountId,
+        action
+      }
+      scopesArray.push(scope)
+    })
+  })
+
   try {
     await consentDb.register(consent)
-    // await scopesDb.register(scopesArray)
+    await scopesDb.register(scopesArray)
   } catch (error) {
     Logger.push(error).error('Error: Unable to create/store consent and scope')
   }
