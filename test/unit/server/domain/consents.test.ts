2df46a88c76da96378c861d71c9f18fd0b099794
akapur99 <abhi.kapur09@gmail.com>
2020-07-14 10:11:46 +0530

    <test> IGNORE fixing jest mocking - inprogress

---
+++ test/unit/server/domain/consents.test.ts
@@ -0,0 +1,85 @@
+/*****
+ License
+ --------------
+ Copyright © 2020 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the 'License') and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ - Abhimanyu Kapur <abhi.kapur09@gmail.com>
+
+ --------------
+ ******/
+import { Request } from '@hapi/hapi'
+// import { consentDb, scopeDb } from '../../../../src/lib/db'
+import { Consent } from '../../../../src/model/consent'
+import { Scope } from '../../../../src/model/scope'
+import { createAndStoreConsent } from '../../../../src/server/domain/consents'
+
+const mockDbs = jest.mock('../../../../src/lib/db')
+
+/*
+ * Mock Request Resources
+ */
+// @ts-ignore
+const request: Request = {
+  headers: {
+    fspiopsource: 'pisp-2342-2233',
+    fspiopdestination: 'dfsp-3333-2123'
+  },
+  params: {
+    id: '1234'
+  }
+}
+
+// @ts-ignore
+const requestNoHeaders: Request = {
+  params: {
+    id: '1234'
+  }
+}
+
+/*
+ * Mock Consent Resources
+ */
+const partialConsent: Consent = {
+  id: '1234',
+  initiatorId: 'pisp-2342-2233',
+  participantId: 'dfsp-3333-2123'
+}
+
+const partialConsent2: Consent = {
+  id: '1234',
+  initiatorId: 'pisp-2342-2234',
+  participantId: 'dfsp-3333-2123'
+}
+
+const completeConsent: Consent = {
+  id: '1234',
+  initiatorId: 'pisp-2342-2233',
+  participantId: 'dfsp-3333-2123',
+  credentialId: '123',
+  credentialType: 'FIDO',
+  credentialStatus: 'PENDING',
+  credentialChallenge: 'xyhdushsoa82w92mzs='
+}
+
+const nullConsent: Consent = null
+
+describe('server/domain/consents', (): void => {
+  it('Should throw an error', async (): Promise<void> => {
+    // TODO: FIll out
+  })
+})
---
+++ test/unit/server/handlers/consents.test.ts
@@ -0,0 +1,84 @@
+/*****
+ License
+ --------------
+ Copyright © 2020 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the 'License') and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ - Abhimanyu Kapur <abhi.kapur09@gmail.com>
+
+ --------------
+ ******/
+import { Request } from '@hapi/hapi'
+import { consentDb, scopeDb } from '../../../../src/lib/db'
+import { Consent } from '../../../../src/model/consent'
+import { Scope } from '../../../../src/model/scope'
+import { post } from '../../../../src/server/handlers/consents'
+import { createAndStoreConsent } from '../../../../src/server/domain/consents'
+
+const mockStoreConsent = jest.fn(createAndStoreConsent)
+
+/*
+ * Mock Request Resources
+ */
+// @ts-ignore
+const request: Request = {
+  headers: {
+    fspiopsource: 'pisp-2342-2233',
+    fspiopdestination: 'dfsp-3333-2123'
+  },
+  params: {
+    id: '1234'
+  },
+  payload: {
+    scopes: [
+      {
+        accountId: '3423',
+        actions: ['acc.getMoney', 'acc.sendMoney']
+      },
+      {
+        accountId: '232345',
+        actions: ['acc.accessSaving']
+      }
+    ]
+  }
+}
+
+// @ts-ignore
+const requestNoHeaders: Request = {
+  params: {
+    id: '1234'
+  }
+}
+
+/*
+ * Mock Consent Resources
+ */
+const partialConsent: Consent = {
+  id: '1234',
+  initiatorId: 'pisp-2342-2233',
+  participantId: 'dfsp-3333-2123'
+}
+
+
+const nullConsent: Consent = null
+
+describe('server/handlers/consents', (): void => {
+
+  it('Should throw an error', async (): Promise<void> => {
+    // TODO: Fill out
+  })
+})
