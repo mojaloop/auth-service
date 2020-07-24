ec0ade5d67649a487bc7e7a7727b4983b7e489ca
akapur99 <abhi.kapur09@gmail.com>
2020-07-24 11:47:10 +0530

    style: indentation of comment

--- src/domain/consents.ts
+++ src/domain/consents.ts
@@ -52,10 +52,10 @@ interface PostConsentPayload {
 }
 
 /**
-   * Validates whether request is valid
-   * by comparing if source header matches participant ID
-   * @param request: request received from switch
-   */
+ * Validates whether request is valid
+ * by comparing if source header matches participant ID
+ * @param request: request received from switch
+ */
 export function isRequestValid (request: Request): boolean {
   const payload = request.payload as PostConsentPayload
   const fspiopSource = request.headers[Enum.Http.Headers.FSPIOP.SOURCE]
