2f4b2ce389a6d9564d6dcec2194c6c83b821add5
Ahan Gupta <ahangupta.96@gmail.com>
2020-08-05 23:13:21 +0800

    Integration tests (#25)
    * Modify gitignore to include DS_Store
    
    * Add database er diagram and attribute trypes & constrants
    
    * Add description to ER diagram
    
    * Fix grammar
    
    * Fix alignment
    
    * Fix table alignment
    
    * Segment scope from consent table, add additional justification
    
    * Delete old png er diagram, inserted new uml specs instead
    
    * Fix proxy renderer link
    
    * Fix typos
    
    * Center ER diagram
    
    * Update attribute types, constraints and descriptions
    
    * Fix data type formatting
    
    * Update ER diagram
    
    * Modify credential attributes to nullable to adhere to new specifications
    
    * Change account_number to account_id
    
    * Change account_number to account_id
    
    * Add justification for Nullable constraints in the schema
    
    * Resolve merge conflict
    
    * Add justification for not storing signatures and modify diagram
    
    * Update lock file
    
    * Remove execcsive quotations in Er Diagram description
    
    * Rename time_stamp field to created_at
    
    * Change type of credential_id to CHAR(256)
    
    * Change description of credential_id
    
    * fix(warning): convert createTableIfNotExists to hasTable followed by createTable to suppress knex warnings.
    
    * chore(style): make the then statement in a new line.
    
    * fix: Incrementally change the contents of the CI build and docker builds to
    uncover where build is failing.
    
    * audit: ignore prototype pollution to circumvent security check in CI build
    
    * fix: change CI workflow to run coverage after unit and integration tests are completed
    
    * config: ignore integration tests whilst running jest --coverage
    
    * fix: remove depedency in CI workflow in between test-coverage and test-integration
    
    * fix: add wait command to CI config.
    
    * fix: typo in CI config
    
    * fix: add wait command to auth-service docker build
    
    * fix: add wait script to ci build
    
    * fix: add convert to executable step before executing js script in CI build
    
    * refactor: remove command from docker-compose, migrate wait to ci build
    
    * fix: add new line to end of dockerfile
    
    * fix: add production prune to dockerfile
    
    * fix: store_artifacts post build in integration test CI workflow
    
    * fix: typo in CI config
    
    * fix: change ENVIRONMENT to DB_ENVIRONMENT for config
    
    Co-authored-by: Raman Mangla <41443552+ramanmangla@users.noreply.github.com>

---
+++ scripts/wait4.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // format version sem-ver
+  // `v{major}.${minor}.${patch}`
+  wait4: 'v0.1.0',
+
+  // How many times should we retry waiting for a service?
+  retries: 10,
+
+  // How many ms to wait before retrying a service connection?
+  waitMs: 2500,
+
+  // services definitions
+  services: [
+    {
+      name: 'my-sql',
+
+      // list of services to wait for
+      wait4: [
+        {
+          description: 'MySQL Database',
+          /* Change host:port accordingly based on default.json attributbes. */
+          uri: 'localhost:3306',
+          method: 'mysql',
+          retries: 30
+        }
+      ]
+    }
+  ]
+}
