46ba56e8761658acc201dd28cfc4a7764b88158c
spikerheado1234 <ahangupta.96@gmail.com>
2020-08-06 20:56:22 +0800

    fix: add config dependent knex instance generation

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -29,12 +29,38 @@
 
 import Knex from 'knex'
 import Config from '../../config/knexfile'
+import { config } from './config'
 import ConsentDB from '../model/consent'
 import ScopeDB from '../model/scope'
 
-const Db: Knex = Knex(Config.test)
-const consentDB: ConsentDB = new ConsentDB(Db)
-const scopeDB: ScopeDB = new ScopeDB(Db)
+function getKnexInstance (): Knex {
+  let Db: Knex
+  switch (config.get('ENVIRONMENT')) {
+    case 'test': {
+      Db = Knex(Config.test as object)
+      break
+    }
+
+    case 'development': {
+      Db = Knex(Config.development as object)
+      break
+    }
+
+    case 'production': {
+      Db = Knex(Config.production as object)
+      break
+    }
+
+    default: {
+      throw new Error('Unexpected environment encountered.')
+    }
+  }
+  return Db
+}
+
+const knexInstance: Knex = getKnexInstance()
+const consentDB: ConsentDB = new ConsentDB(knexInstance)
+const scopeDB: ScopeDB = new ScopeDB(knexInstance)
 
 export {
   consentDB,
