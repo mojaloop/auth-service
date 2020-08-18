834b43a6128137fd74fb8e573f223be45abc5e87
akapur99 <abhi.kapur09@gmail.com>
2020-08-18 15:11:33 +0530

    feat/refactor/perf: Add PutConsentRequest Interface+Improve code readability+Refactor handler logic Remove destructuring of payload from main put function and Add to retrieveUpdateAndPutConsent Change array assignment to destructuring for clarity + perf Remove unneeded try/catch Remove putConsent call and build outgoing request body in Handler Make outgoing thirdPartyRequest call from Handler

--- src/server/handlers/consents/{ID}.ts
+++ src/server/handlers/consents/{ID}.ts
@@ -37,61 +37,80 @@
  --------------
  ******/
 import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi'
-import { Consent } from '~/model/consent'
+import { Consent, ConsentCredential } from '~/model/consent'
 import { Logger } from '@mojaloop/central-services-logger'
+import * as SDKStandardComponents from '@mojaloop/sdk-standard-components'
+import { thirdPartyRequest } from '~/lib/requests'
 import {
   retrieveValidConsent,
   updateConsentCredential,
-  putConsents, ConsentCredential,
-  checkCredentialStatus
+  checkCredentialStatus,
+  buildConsentRequestBody
 } from '~/domain/consents/{ID}'
-import { IncorrectChallengeError, IncorrectStatusError } from '~/domain/errors'
 import { verifySignature } from '~/lib/challenge'
-import { NotFoundError } from '~/model/errors'
 import { Enum } from '@mojaloop/central-services-shared'
+import { CredentialStatusEnum } from '~/model/consent/consent'
+
+interface PutConsentRequest {
+  credential: {
+    id: string;
+    payload: string;
+    // TODO: should this be an enum? Or a string constant?
+    // Maybe we should leave it as a string as we're checking against an enum?
+    status: string;
+    challenge: {
+      signature: string;
+      payload: string;
+    };
+  };
+}
 
 export async function retrieveUpdateAndPutConsent (
-  id: string,
-  challenge: string,
-  credentialStatus: string,
-  signature: string,
-  publicKey: string,
-  requestCredentialId: string,
   request: Request): Promise<void> {
+  const id = request.params.id
+
+  const {
+    credential: {
+      challenge: {
+        signature,
+        payload: challenge
+      },
+      payload: publicKey,
+      id: requestCredentialId,
+      status: credentialStatus
+    }
+  } = request.payload as PutConsentRequest
+
   try {
     const consent: Consent = await retrieveValidConsent(id, challenge)
     /* Checks if incoming credential status is of the correct form */
     checkCredentialStatus(credentialStatus, id)
-    try {
-      if (!verifySignature(challenge, signature, publicKey)) {
-        Logger.push({ consentId: id })
-        Logger.error('Invalid Challenge')
-        /* TODO, make outbound call to PUT consents/{ID}/error
-        to be addressed in ticket number 355 */
-        return
-      }
-      const credential: ConsentCredential = {
-        credentialId: requestCredentialId,
-        credentialStatus: 'ACTIVE',
-        credentialPayload: publicKey
-      }
-      await updateConsentCredential(consent, credential)
 
-      /* Outbound PUT consents/{ID} call */
-      putConsents(consent, signature, publicKey, request)
-    } catch (error) {
-      Logger.push(error)
-      Logger.error('Error: Outgoing call with challenge credential NOT made to PUT consents/' + id)
-      /* TODO, make outbound call to PUT consents/{ID}/error
-      to be addressed in ticket number 355 */
-      return
+    const verifyResult = verifySignature(challenge, signature, publicKey)
+    if (!verifyResult) {
+      // TODO: domain specific error
+      throw new Error('Invalid challenge')
     }
+
+    const credential: ConsentCredential = {
+      credentialType: 'FIDO',
+      credentialChallenge: challenge,
+      credentialId: requestCredentialId,
+      credentialStatus: CredentialStatusEnum.ACTIVE,
+      credentialPayload: publicKey
+    }
+    await updateConsentCredential(consent, credential)
+
+    /* Outbound PUT consents/{ID} call */
+    const consentBody: SDKStandardComponents.PutConsentsRequest = await buildConsentRequestBody(consent, signature, publicKey)
+    await thirdPartyRequest
+      .putConsents(
+        consent.id,
+        consentBody,
+        request.headers[Enum.Http.Headers.FSPIOP.SOURCE]
+      )
   } catch (error) {
-    if (error instanceof IncorrectChallengeError ||
-      error instanceof IncorrectStatusError ||
-      error instanceof NotFoundError) {
-      Logger.push(error)
-    }
+    // TODO: common outbound call here
     Logger.push(error)
     Logger.error('Error in retrieving consent.')
     /* TODO, make outbound call to PUT consents/{ID}/error
@@ -102,16 +121,7 @@ export async function retrieveUpdateAndPutConsent (
 export async function put (
   request: Request,
   h: ResponseToolkit): Promise<ResponseObject> {
-  const id = request.params.id
-  // @ts-ignore
-  const requestPayloadCredential = request.payload.credential
-  const [signature, publicKey, challenge, requestCredentialId, credentialStatus] = [
-    requestPayloadCredential.challenge.signature,
-    requestPayloadCredential.payload,
-    requestPayloadCredential.challenge.payload,
-    requestPayloadCredential.id,
-    requestPayloadCredential.status
-  ]
-  retrieveUpdateAndPutConsent(id, challenge, credentialStatus, signature, publicKey, requestCredentialId, request)
+  // Note: not awaiting promise here
+  retrieveUpdateAndPutConsent(request)
   return h.response().code(Enum.Http.ReturnCodes.ACCEPTED.CODE)
 }
