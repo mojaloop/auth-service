85fb7fdb975fa3266fc43134128119d0ee1e91b1
Paweł Marzec <pm@eoln.eu>
2020-10-13 16:13:31 +0200

    feat(shared/logger): use sdk-standard-components logger

--- test/unit/shared/logger.test.ts
+++ test/unit/shared/logger.test.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /*****
  License
  --------------
  Copyright © 2020 Mojaloop Foundation
- The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License")
+ and you may not use these files except in compliance with the License. You may obtain a copy of the License at
  http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed
+ on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
  Contributors
  --------------
  This is the official list of the Mojaloop project contributors for this file.
@@ -15,23 +19,45 @@
  Gates Foundation organization for an example). Those individuals should have
  their names indented and be marked with a '-'. Email address can be added
  optionally within square brackets <email>.
- * Gates Foundation
- - Name Surname <name.surname@gatesfoundation.com>
 
- * Paweł Marzec <pawel.marzec@modusbox.com>
+ - Paweł Marzec <pawel.marzec@modusbox.com>
+
  --------------
  ******/
-import { logResponse, RequestLogged } from '~/shared/logger'
-import logger from '@mojaloop/central-services-logger'
-import inspect from '~/shared/inspect'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-jest.mock('@mojaloop/central-services-logger', () => ({
-  info: jest.fn()
-}))
+import { RequestLogged, logResponse, logger, createLogger } from '~/shared/logger'
+// import { Logger as SDKLogger } from '@mojaloop/sdk-standard-components'
+import inspect from '~/shared/inspect'
+// import { Logger as SDKLogger } from '@mojaloop/sdk-standard-components'
+
+// import SDK from '@mojaloop/sdk-standard-components'
+// import { mocked } from 'ts-jest/utils'
+
+jest.mock('@mojaloop/sdk-standard-components',
+  jest.fn(() => ({
+    Logger: {
+      Logger: jest.fn(() => ({
+        push: jest.fn(),
+        configure: jest.fn(),
+
+        // log methods
+        log: jest.fn(),
+
+        // generated methods from default levels
+        verbose: jest.fn(),
+        debug: jest.fn(),
+        warn: jest.fn(),
+        error: jest.fn(),
+        trace: jest.fn(),
+        info: jest.fn(),
+        fatal: jest.fn()
+      })),
+      buildStringify: jest.fn()
+    }
+  }))
+)
 
 describe('shared/logger', (): void => {
-  afterEach((): typeof jest => jest.clearAllMocks())
   it('should do nothing if no request', (): void => {
     logResponse(null as unknown as RequestLogged)
     expect(logger.info).not.toBeCalled()
@@ -42,26 +68,77 @@ describe('shared/logger', (): void => {
     const request = { response: { source: 'abc', statusCode: 200 } }
     logResponse(request as RequestLogged)
     expect(spyStringify).toBeCalledWith('abc')
-    expect(logger.info).toBeCalledWith(`AS-Trace - Response: ${JSON.stringify(request.response.source)} Status: ${request.response.statusCode}`)
+    expect(logger.info).toBeCalledWith(
+      `AS-Trace - Response: ${JSON.stringify(request.response.source)} Status: ${request.response.statusCode}`
+    )
   })
 
   it('should log response via inspect', (): void => {
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     jest.mock('~/shared/inspect', () => jest.fn())
+
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    const spyStringify = jest.spyOn(JSON, 'stringify').mockImplementationOnce(() => { throw new Error('parse-error') })
+    const spyStringify = jest.spyOn(JSON, 'stringify').mockImplementationOnce(
+      () => { throw new Error('parse-error') }
+    )
+
     const request = { response: { source: 'abc', statusCode: 200 } }
     logResponse(request as RequestLogged)
     expect(spyStringify).toBeCalled()
-    expect(logger.info).toBeCalledWith(`AS-Trace - Response: ${inspect(request.response.source)} Status: ${request.response.statusCode}`)
+    expect(logger.info).toBeCalledWith(
+      `AS-Trace - Response: ${inspect(request.response.source)} Status: ${request.response.statusCode}`
+    )
   })
 
   it('should log if there is no request.response', (): void => {
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    const spyStringify = jest.spyOn(JSON, 'stringify').mockImplementationOnce(() => null as unknown as string)
+    const spyStringify = jest.spyOn(JSON, 'stringify').mockImplementationOnce(
+      () => null as unknown as string
+    )
     const request = { response: { source: 'abc', statusCode: 200 } }
     logResponse(request as RequestLogged)
     expect(spyStringify).toBeCalled()
     expect(logger.info).toBeCalledWith(`AS-Trace - Response: ${request.response.toString()}`)
+  })
+
+  describe('Logger class', () => {
+    it('should be able to create default logger', () => {
+      const log = createLogger()
+      // basic methods
+      expect(typeof log.push).toEqual('function')
+      expect(typeof log.configure).toEqual('function')
+
+      // log methods
+      expect(typeof log.log).toEqual('function')
+
+      // generated methods from default levels
+      expect(typeof log.verbose).toEqual('function')
+      expect(typeof log.debug).toEqual('function')
+      expect(typeof log.warn).toEqual('function')
+      expect(typeof log.error).toEqual('function')
+      expect(typeof log.trace).toEqual('function')
+      expect(typeof log.info).toEqual('function')
+      expect(typeof log.fatal).toEqual('function')
+    })
+  })
+
+  describe('logger default instance', () => {
+    it('should have proper layout', () => {
+      // basic methods
+      expect(typeof logger.push).toEqual('function')
+      expect(typeof logger.configure).toEqual('function')
+
+      // log methods
+      expect(typeof logger.log).toEqual('function')
+
+      // generated methods from default levels
+      expect(typeof logger.verbose).toEqual('function')
+      expect(typeof logger.debug).toEqual('function')
+      expect(typeof logger.warn).toEqual('function')
+      expect(typeof logger.error).toEqual('function')
+      expect(typeof logger.trace).toEqual('function')
+      expect(typeof logger.info).toEqual('function')
+      expect(typeof logger.fatal).toEqual('function')
+    })
   })
 })
