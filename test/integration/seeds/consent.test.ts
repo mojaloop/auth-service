160b684607d86c19ae81c3188725aa73d17a1f4c
Kevin Leyow <kleyow@gmail.com>
2021-06-25 05:48:21 -0500

    feat: update db schema for clientDataJSON and attestationObject (#73)
    * feat: update db schema for clientDataJSON and attestationObject
    
    * chore: update integration tests
    
    * chore: quell vulnerabilty warnings for now

--- test/integration/seeds/consent.test.ts
+++ test/integration/seeds/consent.test.ts
@@ -53,7 +53,9 @@ describe('testing Consent table', (): void => {
       credentialStatus: null,
       credentialPayload: null,
       credentialChallenge: null,
-      revokedAt: null
+      revokedAt: null,
+      attestationObject: null,
+      clientDataJSON: null
     })
     expect(users[1]).toMatchObject({
       id: '124',
@@ -65,7 +67,32 @@ describe('testing Consent table', (): void => {
       credentialStatus: 'PENDING',
       credentialPayload: null,
       credentialChallenge: 'string_representing_challenge_a',
-      revokedAt: null
+      revokedAt: null,
+      attestationObject: 'o2NmbXRmcGFja2VkZ2F0dFN0bXSjY2FsZyZjc2lnWEcwRQIgHq9' +
+      'JKpi/bFnnu0uVV+k6JjHfBcFwWRRCXJWlejgzJLUCIQD2iOONGXebOCxq37UqvumxC/d' +
+      'Jz1a3U9F1DaxVMFnzf2N4NWOBWQLBMIICvTCCAaWgAwIBAgIECwXNUzANBgkqhkiG9w0' +
+      'BAQsFADAuMSwwKgYDVQQDEyNZdWJpY28gVTJGIFJvb3QgQ0EgU2VyaWFsIDQ1NzIwMDY' +
+      'zMTAgFw0xNDA4MDEwMDAwMDBaGA8yMDUwMDkwNDAwMDAwMFowbjELMAkGA1UEBhMCU0U' +
+      'xEjAQBgNVBAoMCVl1YmljbyBBQjEiMCAGA1UECwwZQXV0aGVudGljYXRvciBBdHRlc3R' +
+      'hdGlvbjEnMCUGA1UEAwweWXViaWNvIFUyRiBFRSBTZXJpYWwgMTg0OTI5NjE5MFkwEwY' +
+      'HKoZIzj0CAQYIKoZIzj0DAQcDQgAEIRpvsbWJJcsKwRhffCrjqLSIEBR5sR7/9VXgfZd' +
+      'RvSsXaiUt7lns44WZIFuz6ii/j9f8fadcBUJyrkhY5ZH8WqNsMGowIgYJKwYBBAGCxAo' +
+      'CBBUxLjMuNi4xLjQuMS40MTQ4Mi4xLjEwEwYLKwYBBAGC5RwCAQEEBAMCBDAwIQYLKwY' +
+      'BBAGC5RwBAQQEEgQQFJogIY72QTOWuIH41bfx9TAMBgNVHRMBAf8EAjAAMA0GCSqGSIb' +
+      '3DQEBCwUAA4IBAQA+/qPfPSrgclePfgTQ3VpLaNsBr+hjLhi04LhzQxiRGWwYS+vB1TO' +
+      'iPXeLsQQIwbmqQU51doVbCTaXGLNIr1zvbLAwhnLWH7i9m4ahCqaCzowtTvCQ7VBUGP5' +
+      'T1M4eYnoo83IDCVjQj/pZG8QYgOGOigztGoWAf5CWcUF6C0UyFbONwUcqJEl2QLToa/7' +
+      'E8VRjm4W46IAUljYkODVZASv8h3wLROx9p5TSBlSymtwdulxQe/DKbfNSvM3edA0up+E' +
+      'IJKLOOU+QTR2ZQV46fEW1/ih6m8vcaY6L3NW0eYpc7TXeijUJAgoUtya/vzmnRAecuY9' +
+      'bncoJt8PrvL2ir2kDaGF1dGhEYXRhWMRJlg3liA6MaHQ0Fw9kdmBbj+SuuaKGMseZXPO' +
+      '6gx2XY0EAAAAEFJogIY72QTOWuIH41bfx9QBAX8aQc8WgIOiYzoRIKbTYJdlzMZ/8zo3' +
+      'ZiIL3Rvh/ONfr9kZtudCwYO49tWVkjgJGyJSpoo6anRBVJGda0Lri3aUBAgMmIAEhWCB' +
+      '0Zo9xAj7V50Tu7Hj8F5Wo0A3AloIpsVDSY2icW9eSwiJYIH79t0O2hnPDguuloYn2eSd' +
+      'R7caaZd/Ffnmk4vyOATab',
+      clientDataJSON: '{"type":"webauthn.create","challenge":"MgA3ADgANQBjADIAZ' +
+      'AA5ADkAYQA0AGMAMQA5AGQAMQBhADgANwBkADMANABmAGQAMABjADEAMABhAGQAMABiA' +
+      'DUAMgA3ADIAMQBjAGYAMwBjADgAMAAyADgAOABjADIAOQBkAGEANQBiADAAZQBiAGUAZ' +
+      'gA2ADcAOAAzADQAMAA","origin":"http://localhost:5000","crossOrigin":false}'
     })
     expect(users[2]).toMatchObject({
       id: '125',
@@ -77,7 +104,32 @@ describe('testing Consent table', (): void => {
       credentialStatus: 'VERIFIED',
       credentialPayload: 'string_representing_public_key_a',
       credentialChallenge: 'string_representing_challenge_b',
-      revokedAt: null
+      revokedAt: null,
+      attestationObject: 'o2NmbXRmcGFja2VkZ2F0dFN0bXSjY2FsZyZjc2lnWEcwRQIgHq9' +
+      'JKpi/bFnnu0uVV+k6JjHfBcFwWRRCXJWlejgzJLUCIQD2iOONGXebOCxq37UqvumxC/d' +
+      'Jz1a3U9F1DaxVMFnzf2N4NWOBWQLBMIICvTCCAaWgAwIBAgIECwXNUzANBgkqhkiG9w0' +
+      'BAQsFADAuMSwwKgYDVQQDEyNZdWJpY28gVTJGIFJvb3QgQ0EgU2VyaWFsIDQ1NzIwMDY' +
+      'zMTAgFw0xNDA4MDEwMDAwMDBaGA8yMDUwMDkwNDAwMDAwMFowbjELMAkGA1UEBhMCU0U' +
+      'xEjAQBgNVBAoMCVl1YmljbyBBQjEiMCAGA1UECwwZQXV0aGVudGljYXRvciBBdHRlc3R' +
+      'hdGlvbjEnMCUGA1UEAwweWXViaWNvIFUyRiBFRSBTZXJpYWwgMTg0OTI5NjE5MFkwEwY' +
+      'HKoZIzj0CAQYIKoZIzj0DAQcDQgAEIRpvsbWJJcsKwRhffCrjqLSIEBR5sR7/9VXgfZd' +
+      'RvSsXaiUt7lns44WZIFuz6ii/j9f8fadcBUJyrkhY5ZH8WqNsMGowIgYJKwYBBAGCxAo' +
+      'CBBUxLjMuNi4xLjQuMS40MTQ4Mi4xLjEwEwYLKwYBBAGC5RwCAQEEBAMCBDAwIQYLKwY' +
+      'BBAGC5RwBAQQEEgQQFJogIY72QTOWuIH41bfx9TAMBgNVHRMBAf8EAjAAMA0GCSqGSIb' +
+      '3DQEBCwUAA4IBAQA+/qPfPSrgclePfgTQ3VpLaNsBr+hjLhi04LhzQxiRGWwYS+vB1TO' +
+      'iPXeLsQQIwbmqQU51doVbCTaXGLNIr1zvbLAwhnLWH7i9m4ahCqaCzowtTvCQ7VBUGP5' +
+      'T1M4eYnoo83IDCVjQj/pZG8QYgOGOigztGoWAf5CWcUF6C0UyFbONwUcqJEl2QLToa/7' +
+      'E8VRjm4W46IAUljYkODVZASv8h3wLROx9p5TSBlSymtwdulxQe/DKbfNSvM3edA0up+E' +
+      'IJKLOOU+QTR2ZQV46fEW1/ih6m8vcaY6L3NW0eYpc7TXeijUJAgoUtya/vzmnRAecuY9' +
+      'bncoJt8PrvL2ir2kDaGF1dGhEYXRhWMRJlg3liA6MaHQ0Fw9kdmBbj+SuuaKGMseZXPO' +
+      '6gx2XY0EAAAAEFJogIY72QTOWuIH41bfx9QBAX8aQc8WgIOiYzoRIKbTYJdlzMZ/8zo3' +
+      'ZiIL3Rvh/ONfr9kZtudCwYO49tWVkjgJGyJSpoo6anRBVJGda0Lri3aUBAgMmIAEhWCB' +
+      '0Zo9xAj7V50Tu7Hj8F5Wo0A3AloIpsVDSY2icW9eSwiJYIH79t0O2hnPDguuloYn2eSd' +
+      'R7caaZd/Ffnmk4vyOATab',
+      clientDataJSON: '{"type":"webauthn.create","challenge":"MgA3ADgANQBjADIAZ' +
+      'AA5ADkAYQA0AGMAMQA5AGQAMQBhADgANwBkADMANABmAGQAMABjADEAMABhAGQAMABiA' +
+      'DUAMgA3ADIAMQBjAGYAMwBjADgAMAAyADgAOABjADIAOQBkAGEANQBiADAAZQBiAGUAZ' +
+      'gA2ADcAOAAzADQAMAA","origin":"http://localhost:5000","crossOrigin":false}'
     })
   })
 })
@@ -106,7 +158,9 @@ describe('testing that constraints are enforced in the consent table', (): void 
       credentialStatus: null,
       credentialPayload: null,
       credentialChallenge: null,
-      revokedAt: null
+      revokedAt: null,
+      attestationObject: null,
+      clientDataJSON: null
     })).rejects.toThrow()
     /* Tests for non-nullity */
     await expect(db.from('Consent').insert({
@@ -119,7 +173,9 @@ describe('testing that constraints are enforced in the consent table', (): void 
       credentialStatus: null,
       credentialPayload: null,
       credentialChallenge: null,
-      revokedAt: null
+      revokedAt: null,
+      attestationObject: null,
+      clientDataJSON: null
     })).rejects.toThrow()
   })
   it('should properly enforce the non-nullable constraint for initiatorId', async (): Promise<void> => {
@@ -132,7 +188,9 @@ describe('testing that constraints are enforced in the consent table', (): void 
       credentialType: null,
       credentialStatus: null,
       credentialPayload: null,
-      credentialChallenge: null
+      credentialChallenge: null,
+      attestationObject: null,
+      clientDataJSON: null
     })).rejects.toThrow()
   })
   it('should properly enforce the non-nullable constraint for status', async (): Promise<void> => {
@@ -147,7 +205,9 @@ describe('testing that constraints are enforced in the consent table', (): void 
       credentialStatus: null,
       credentialPayload: null,
       credentialChallenge: null,
-      revokedAt: null
+      revokedAt: null,
+      attestationObject: null,
+      clientDataJSON: null
     })).rejects.toThrow()
   })
   it('should properly enforce the non-nullable constraint for participantId', async (): Promise<void> => {
@@ -162,7 +222,9 @@ describe('testing that constraints are enforced in the consent table', (): void 
       credentialStatus: null,
       credentialPayload: null,
       credentialChallenge: null,
-      revokedAt: null
+      revokedAt: null,
+      attestationObject: null,
+      clientDataJSON: null
     })).rejects.toThrow()
   })
 })
