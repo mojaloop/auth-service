dd24705ab35a4f0d6ea6800e1e7d9762acd5ab12
Juan Correa <gibaros@users.noreply.github.com>
2025-01-29 14:38:26 -0500

    chore: maintenance updates
    -fix vulnerabilities
    -update dependencies
    -update to NodeJS v18.20.4
    -update CODEOWNERS
    -update LICENSE.md

--- .ncurc.js
+++ .ncurc.js
@@ -5,9 +5,6 @@ module.exports = {
     // Upgrading past redis@3 to the next major version introduces a lot of breaking changes.
     'redis',
     '@types/redis',
-    // Upgrading sqlite past 5.0.2 seems to introduce sh: 1: node-pre-gyp: not found.
-    // Investigation needed.
-    'sqlite3',
     // Upgrading fido2-lib past @2.8.3 seems to break tests with error message
     // `error parsing ASN.1`. Investigation needed.
     'fido2-lib',
